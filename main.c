8950e5ba087928b7d935c29a49ce7923e9c8272e
roocheerah <roocheerah@users.noreply.github.com>
2015-04-19 10:17:18 -0700

    Update main.c
    Haven't tested it yet, but I did some commenting if you guys want to look at it!
    Sources used for String Concatenations: http://stackoverflow.com/questions/308695/how-to-concatenate-const-literal-strings-in-c

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "driverlib/sysctl.h"
 #include "drivers/rit128x96x4.h"
 #include "header.h"
-
+#include <string.h>
 //*****************************************************************************
 //
 // The error routine that is called if the driver library encounters an error.
@@ -21,8 +21,9 @@ __error__(char *pcFilename, unsigned long ulLine)
 #define TRUE 1
 #define FALSE 0
 
-
-
+int currentPos = 0;
+int selectPressed = 0;
+char* menu = "Main Menu:                            ";
 //*****************************************************************************
 //
 //
@@ -49,9 +50,69 @@ main(void)
     
     // main loop
     while(TRUE) {
+      int sel = keymaster();     
+      char* options = options_creator();
+      char* menu0 = strcat*(menu, options[0]);
+      char* menu1 = strcat*(menu, options[1]);
+      char* menu2 = strcat*(menu, options[2]);
+      //if select option is pressed, main menu is displayed
+      if(sel == 5){
+        selectPressed = 1;  
+        display_init();
+        //when the user selects select, the main menu initially displays Easy
+        //char* menu = "Main Menu:                            1 : Easy\0";
+        //char* menu0 = strcat*(menu, options[0]);
+        display_string(menu0);
+      }
       
+      //This scroll menu is like a state machine
+      //Here, currentPos refers to the "State" of the menu
+      //so, if Easy is displayed on the 2nd line, ==> currentPos = 0
+      // if Medium is displayed on the 2nd Line ==> currentPos = 1
+      // if Hard if displayed ==> currentPos = 2
+      //
+      //As we only want the user to use the up and down keys when
+      //the user has "chosen" to play the game, selectPressed flag needs
+      //to be checked
+      //
+      
+      if(selectPressed){
+        int pressed = keymaster();
+        if((pressed == 2) && (currentPos == 0)){
+            display_init();
+            display_string(menu1);
+            currentPos = 1;
+        }else if((pressed == 2) && (currentPos == 1)){
+            display_init();
+            display_string(menu2);
+            currentPos = 2;
+        }else if((pressed == 1) && (currentPos == 0)){
+            display_init();
+            display_string(menu2);
+            currentPos = 2;
+        }else if((pressed == 1) && (currentPos == 1)){
+            display_init();
+            display_string(menu0);
+            currentPos = 0;
+        }else if((pressed == 2) && (currentPos == 2)){
+            display_init();
+            display_string(menu0);
+            currentPos = 0;
+        }else if((pressed == 1) && (currentPos == 2)){
+            display_init();
+            display_string(menu1);
+            currentPos = 1;
+        }
+      }
       fresh_key();
     }
+}
+
+//makes a character array of the three levels of the game.
+//options is an array of three character pointers
+char* options_creator() {
+    char* options[3] = {"1: Easy\0", "2: Medium\0", "3: Hard\0"}; 
+    return options;
 }
 
 void LED_init() {
