d7fcc8249329d86bdf0c33143747b16f5b5a53c8
Eeshan Londhe <eeshan@uw.edu>
2015-04-20 02:51:00 -0700

    Update header.h

--- header.h
+++ header.h
@@ -1,4 +1,5 @@
 #include "inc/lm3s8962.h"
+#include "drivers/rit128x96x4.h"
 
 #ifndef HEADER_H
 #define HEADER_H
@@ -18,5 +19,15 @@ void portBD_init();
 void display_write(int reset, char w);
 void display_string(char* g);
 void clear_display();
+void shiftRight();
+void shiftLeft();
+void cursorLeft();
+void cursorRight();
+void mainMenu();
+void game(int level);
+void startGame(int level);
+int checkInput(char input, int key);
+char string_combos();
+void goToStateOne();
 
 #endif
