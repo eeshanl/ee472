e474bc76ae2bc2c280999b25232ef267ed3d18c8
eeshanl <eeshan@uw.edu>
2015-05-11 18:10:49 -0700

    edits

--- Lab3/main.c
+++ Lab3/main.c
@@ -1,6 +1,6 @@
 #include "header.h"
 //*****************************************************************************
-//main.c creates a game of 'Guess the Height' using the sensor and displayed 
+//main.c creates a game of 'Guess the Height' using the sensor and displayed
 //its menu on the LCD Display using the LM3S8962 Micro-controller. A player
 //has multiple levels of difficulties (Easy, Medium, Hard) to choose from in the
 //main menu of the game
@@ -33,35 +33,35 @@ main(void) {
   ADC_init();
   //Initializes the keys on the keypad on the Stellaris Board
   key_init();
-  //Initializes Timer0 and enables Timer0 interrupts 
+  //Initializes Timer0 and enables Timer0 interrupts
   timer0_init();
-  //Initializes Timer1 and enables Timer1 interrupts 
+  //Initializes Timer1 and enables Timer1 interrupts
   timer1_init();
-  //Initializes Timer2 and enables Timer2 interrupts 
+  //Initializes Timer2 and enables Timer2 interrupts
   timer2_init();
-  //Initializes GPIO PORT E and enables its interrupts 
+  //Initializes GPIO PORT E and enables its interrupts
   init_GPIOE();
   //Initializes GPIO PORT F and enables its interrupts
   init_GPIOF();
   //Initializes the LCD Display
-  display_init(); 
+  display_init();
   //Inital display that shows up to start the game
   char* str = "Let's Play GTH!                         Press sel to go!\0";
   // prints out the initial message
-  display_string(str); 
+  display_string(str);
   // main loop
   while(TRUE) {
     // main state representing the easy screen of the main menu
-    static int mainState = 0; 
+    static int mainState = 0;
     // if the user presses select, game should start
-    if (mainState == 0 && keymaster() == 5) { 
+    if (mainState == 0 && keymaster() == 5) {
       //clears the display on LCD to allow the new game's menu to show up
       clear_display();
       //the first part of the scroll down menu for the game
       str = "Main Menu:                              Easy\0";
       display_string(str);
       // changes mainState to -1 so that after this time the mainMenu can be initiated
-      mainState = -1; 
+      mainState = -1;
     }
     //the the mainstate is not 0, show the main meu for the game of GTH
     //as the user has pressed select already
@@ -69,11 +69,6 @@ main(void) {
       mainMenuForGTH(); // starts the main menu
     }
   }
-<<<<<<< HEAD
-
-
-=======
->>>>>>> bb73b66fa9d0c5741986b08b7d7a54ad73a61c98
 }
 
 //delay takes in an unsigned long integer as a paramter
